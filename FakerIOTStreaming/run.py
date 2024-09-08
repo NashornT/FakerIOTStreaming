3cafb863689b0920d981320f3a774e13aa8b3850
Gustavo Barboza <Huedor123@gmail.com>
2024-09-08 16:50:27 -0300

    :art: Improve structure

--- FakerIOTStreaming/run.py
+++ FakerIOTStreaming/run.py
@@ -1,5 +1,11 @@
 from faker_iot import FakerIot
+import os
+# Definir o caminho das credenciais do Google Cloud
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r""
 
-run_faker = FakerIot(time=1)
+
+run_faker = FakerIot(project_id="",
+                     topic_id="",
+                     time=4)
 
 run_faker.start_streaming()
