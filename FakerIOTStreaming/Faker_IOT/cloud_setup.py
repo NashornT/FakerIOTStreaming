d109bd5c3a1f195df31006dae338617f13ccf4ed
gustavo <huedor123@gmail.com>
2024-10-17 20:51:10 -0300

    :art: Add Consumer Code.

---
+++ FakerIOTStreaming/Faker_IOT/cloud_setup.py
@@ -0,0 +1,8 @@
+import os
+
+# Definir o caminho das credenciais do Google Cloud
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r""
+
+
+project_id = "" # Nome do seu projeto
+topic_id = "" # Nome do seu Topico
